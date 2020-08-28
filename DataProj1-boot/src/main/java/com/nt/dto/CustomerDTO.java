3b11c3837aae5b6e8a922cde4d35516cb01ff912
Mukesh Patel <patelmukesh325@gmail.com>
2020-08-28 22:57:23 +0530

    spring data-CrudRepository

--- DataProj1-boot/src/main/java/com/nt/dto/CustomerDTO.java
+++ DataProj1-boot/src/main/java/com/nt/dto/CustomerDTO.java
@@ -1,8 +1,10 @@
 package com.nt.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CustomerDTO {
 	public CustomerDTO() {
 		System.out.println("CustomerDTO::0-param Constructor");
